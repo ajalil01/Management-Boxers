e8b1fcdc99a71d6b86a2d1868f075fe1826f9e27
Jalil <abdeldjalilbenzid@gmail.com>
2026-04-18 08:35:00 +0100

    some cors and  .env problems

--- backend/src/main.py
+++ backend/src/main.py
@@ -20,7 +20,7 @@ app = FastAPI(title="Management Boxers API")
 # ---------------- CORS ----------------
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=os.getenv("ALLOWED_ORIGINS", "").split(","),
+    allow_origins=["*"],    # allow_origins=os.getenv("ALLOWED_ORIGINS", "").split(","),
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["Authorization","Content-Type","Accept"]
