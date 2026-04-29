0eae4faa2584b2b24fab2344bf4f5ce77330ed77
Jalil <abdeldjalilbenzid@gmail.com>
2026-04-29 18:46:07 +0100

    changing the config

--- frontend/src/api/config.ts
+++ frontend/src/api/config.ts
@@ -1,2 +1,2 @@
-export const API_BASE_URL = "https://management-boxers-faho.onrender.com";
-export const IMAGE_URL = "https://management-boxers-faho.onrender.com"
+export const API_BASE_URL = "https://management-boxers-o4gt.onrender.com";
+export const IMAGE_URL = "https://management-boxers-o4gt.onrender.com"
